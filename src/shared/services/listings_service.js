1ed8f84d5c73f49bdf5bbb88f5f8cd10ae63ea41
Michael Dever <michael@showoff.ie>
2017-10-04 12:05:12 +0100

    Add listings API

--- src/shared/services/listings_service.js
+++ src/shared/services/listings_service.js
@@ -1,10 +1,26 @@
 import client from '../libraries/client';
 
-const listingsURL = '/api/v1/listings';
+const listingsURL = '/api/v1/listings/';
 
 class ListingsService {
-  static listings() {
-    return bearer_client.get(listingsURL);
+  static index() {
+    return client.get(listingsURL);
+  }
+
+  static show(id) {
+    return client.get(listingsURL + id);
+  }
+
+  static create(params) {
+    return client.post(listingsURL, params);
+  }
+
+  static update(id, params) {
+    return client.put(listingsURL + id, params);
+  }
+
+  static destroy(id) {
+    return client.delete(listingsURL + id);
   }
 }
 
