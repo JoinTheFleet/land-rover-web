d2f7ac59771523fd0170b4373467439ebb1262d7
Michael Dever <michael@showoff.ie>
2017-10-06 17:28:20 +0100

    User reviews service

---
+++ src/shared/services/user_reviews_service.js
@@ -0,0 +1,22 @@
+import client from '../libraries/client';
+import Service from './service';
+
+class UserReviewsService extends Service {
+  static get baseURL() {
+    return '/api/v1/users/:id/reviews/';
+  }
+
+  static index(id, params) {
+    return client.get(this.baseURL.replace(':id', id), {
+      params: params
+    });
+  }
+
+  static get actions() {
+    return {
+      index: true,
+    }
+  }
+}
+
+export default UserReviewsService;
