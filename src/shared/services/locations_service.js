096fcdce1c40a1210120965fffa8fad26b7f1916
Michael Dever <michael@showoff.ie>
2017-10-09 14:38:00 +0100

    Location service

---
+++ src/shared/services/locations_service.js
@@ -0,0 +1,52 @@
+import client from '../libraries/client';
+import Service from './service';
+
+class LocationsService extends Service {
+  static get baseURL() {
+    return '/api/v1/search/locations/';
+  }
+
+  static create(latitude, longitude, term) {
+    let search = {};
+
+    if (term) {
+      search.term = term;
+    }
+
+    if (latitude && longitude) {
+      search.latitude = latitude;
+      search.longitude = longitude
+    }
+
+    return client.post(this.baseURL, {
+      search: search
+    });
+  }
+
+  static location(latitude, longitude) {
+    let search = {};
+
+    if (latitude && longitude) {
+      search.latitude = latitude;
+      search.longitude = longitude
+    }
+
+    return client.post(this.baseURL, {
+      search: search
+    });
+  }
+
+  static term(term) {
+    let search = {};
+
+    if (term) {
+      search.term = term;
+    }
+
+    return client.post(this.baseURL, {
+      search: search
+    })
+  }
+}
+
+export default LocationsService;
