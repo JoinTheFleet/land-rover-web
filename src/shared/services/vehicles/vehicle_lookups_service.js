b467650f2c8a77d9e7bbbf65510c552f8c4ae9c6
Michael Dever <michael@showoff.ie>
2017-10-10 15:52:14 +0100

    Vehicle services

--- src/shared/services/vehicles/vehicle_lookups_service.js
+++ src/shared/services/vehicles/vehicle_lookups_service.js
@@ -1,5 +1,5 @@
-import client from '../libraries/client';
-import Service from './service';
+import client from '../../libraries/client';
+import Service from '../service';
 
 class VehicleLookupsService extends Service {
   static get baseURL() {
