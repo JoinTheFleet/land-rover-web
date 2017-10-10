3f1a2e54abfc83dce8cbd2d93605c034024f4869
Michael Dever <michael@showoff.ie>
2017-10-10 14:16:25 +0100

    Booking surveys

---
+++ src/shared/services/booking_survey_issues_service.js
@@ -0,0 +1,38 @@
+import client from '../libraries/client';
+import Service from './service';
+
+class BookingSurveyIssuesService extends Service {
+  static get baseURL() {
+    return '/api/v1/bookings/:booking_id/surveys/:survey_id/issues/';
+  }
+
+  static surveyURL(booking_id, survey_id, id) {
+    let url = this.baseURL.replace(':booking_id', booking_id).replace(':survey_id', survey_id);
+
+    if (id) {
+      url += id;
+    }
+
+    return url;
+  }
+
+  static index(booking_id, survey_id, params) {
+    return client.get(this.surveyURL(booking_id, survey_id), {
+      params: params
+    });
+  }
+
+  static create(booking_id, survey_id, title, caption, image_url) {
+    return client.post(this.surveyURL(booking_id, survey_id), {
+      title: title,
+      caption: caption,
+      image_url: image_url
+    });
+  }
+
+  static destroy(booking_id, survey_id, id) {
+    return client.delete(this.surveyURL(booking_id, survey_id, id));
+  }
+}
+
+export default BookingSurveyIssuesService;
---
+++ src/shared/services/booking_surveys_service.js
@@ -0,0 +1,16 @@
+import client from '../libraries/client';
+import Service from './service';
+
+class BookingSurveysService extends Service {
+  static get baseURL() {
+    return '/api/v1/bookings/:booking_id/surveys/';
+  }
+
+  static index(booking_id, params) {
+    return client.get(this.baseURL.replace(':booking_id', booking_id), {
+      params: params
+    });
+  }
+}
+
+export default BookingSurveysService;
