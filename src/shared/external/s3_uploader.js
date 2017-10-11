12bb15f6c0ea8410bfc60cef02cd77b2e5bf016b
Michael Dever <michael@showoff.ie>
2017-10-11 12:02:54 +0100

    Add S3 Uploader

---
+++ src/shared/external/s3_uploader.js
@@ -0,0 +1,36 @@
+import AWS from 'aws-sdk';
+import LocalizationService from '../libraries/localization_service'
+import Constants from '../../miscellaneous/constants'
+
+class S3Uploader {
+  static upload(file, folder_name) {
+    AWS.config.update({
+      region: process.env.REACT_APP_AWS_S3_REGION,
+      credentials: new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: process.env.REACT_APP_AWS_COGNITO_POOL_ID
+      })
+    });
+
+    let s3 = new AWS.S3({
+      params: {
+        Bucket: process.env.REACT_APP_AWS_S3_BUCKET_NAME
+      }
+    });
+
+    let bucketFolderName = Constants.s3BucketFolderName(folder_name);
+
+    if (!bucketFolderName || bucketFolderName.length < 1) {
+      return Promise.reject(new Error(LocalizationService.formatMessage('aws.s3.invalid_bucket_folder_name')));
+    }
+    else {
+      return s3.upload({
+        Key: `${bucketFolderName || ''}/${file.name}-${Date.now()}`,
+        Body: file,
+        ContentType: file.type,
+        ACL: "public-read"
+      }).promise();
+    }
+  }
+}
+
+export default S3Uploader;
