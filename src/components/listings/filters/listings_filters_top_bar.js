c3099edc671f20f800c330032fb4aa8d5d983d91
Michael Dever <michael@showoff.ie>
2017-10-19 14:18:35 +0100

    Refactor sort dropdown

--- src/components/listings/filters/listings_filters_top_bar.js
+++ src/components/listings/filters/listings_filters_top_bar.js
@@ -6,6 +6,7 @@ import LocalizationService from '../../../shared/libraries/localization_service'
 import PropTypes from 'prop-types';
 
 import ListingsFilters from './listings_filters';
+import SortDropdown from './sort_dropdown';
 
 const SORT_FILTERS = ['price', 'name', 'rating', 'distance']
 
@@ -24,26 +25,6 @@ class ListingsFiltersTopBar extends Component {
     this.setState((prevState) => ( { filtersOpen: !prevState.filtersOpen } ));
   }
 
-  sortButton() {
-    return (
-      <Dropdown onSelect={this.props.handleSortToggle}
-                pullRight={true}
-                key='search-sort'
-                id='search-sort'>
-        <Dropdown.Toggle className='secondary-color white-text fs-12 selectListingsSortByBtn'>
-          { LocalizationService.formatMessage(`listings.sort.${this.props.selectedSort}`) }
-        </Dropdown.Toggle>
-        <Dropdown.Menu>
-          {
-            SORT_FILTERS.map((filter) => {
-              return <MenuItem eventKey={filter} active={filter === this.props.selectedSort}>{ LocalizationService.formatMessage(`listings.sort.${filter}`) }</MenuItem>
-            })
-          }
-        </Dropdown.Menu>
-      </Dropdown>
-    )
-  }
-
   render() {
     return (
       <div id="filters_top_bar" className="smoke-grey">
@@ -52,8 +33,7 @@ class ListingsFiltersTopBar extends Component {
            { LocalizationService.formatMessage('application.filter') }
         </a>
         <div className="pull-right">
-          <span className="tertiary-text-color text-uppercase">{ LocalizationService.formatMessage('application.sort_by') }</span>
-          { this.sortButton() }
+          <SortDropdown handleSortToggle={this.props.handleSortToggle} selectedSort={this.props.selectedSort}/>
         </div>
         <ListingsFilters open={ this.state.filtersOpen }
                          handleFilterToggle={ this.props.handleFilterToggle }
