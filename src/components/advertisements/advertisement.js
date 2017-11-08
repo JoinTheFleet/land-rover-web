d56c8839fe21474f911cc07cd9933f126757da66
Michael Dever <michael@showoff.ie>
2017-11-08 18:36:13 +0000

    Advertisements

---
+++ src/components/advertisements/advertisement.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { Link } from 'react-router-dom';
+
+import PropTypes from 'prop-types';
+
+export default class Advertisement extends Component {
+  render() {
+    let advertisement = this.props.advertisement;
+
+    return (
+      <div>
+        <div className='col-xs-12 listing-list'>
+          <div className={ 'listing-item col-xs-12 ' + (this.props.additionalClasses || '') }>
+            <div>
+              <div className="listing-item-photo-and-title">
+                <Link to={{
+                        pathname: `${advertisement.url}?token=${this.props.accessToken}`,
+                        
+                      }}
+                      target='_blank'>
+                  <img alt='' src={ advertisement.images.original_url } />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+Advertisement.propTypes = {
+  advertisement: PropTypes.string,
+  accessToken: PropTypes.string
+}
