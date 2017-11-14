78595347560a16d7d06e1b55cf038be78595ba0d
Pedro Miguel Dias de Matos <pedro@showoff.ie>
2017-11-14 16:51:21 +0000

    Added Alerts modal showing after log in (list alerts and mark them as read).

---
+++ src/components/alerts/alerts.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import Alert from 'react-s-alert';
+
+import Modal from '../miscellaneous/modal';
+
+import AlertsService from '../../shared/services/alerts_service';
+import LocalizationService from '../../shared/libraries/localization_service';
+
+import checkIcon from '../../assets/images/check.png';
+
+export default class Alerts extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: false,
+      alerts: []
+    };
+
+    this.fetchAlerts = this.fetchAlerts.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+    this.markAlertAsRead = this.markAlertAsRead.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchAlerts();
+  }
+
+  toggleModal() {
+    this.setState(prevState => ({ open: !prevState.open }));
+  }
+
+  fetchAlerts() {
+    AlertsService.sent()
+                 .then(response => {
+                   const alerts = response.data.data.alerts;
+                   this.setState({ alerts: alerts, open: alerts.length > 0 });
+                 })
+                 .catch(error => { Alert.error(error.response.data.message); });
+  }
+
+  markAlertAsRead(alertId) {
+    AlertsService.update(alertId, { status: 'sent' })
+                 .then(response => {
+                   let alerts = this.state.alerts;
+                   alerts.splice(this.state.alerts.findIndex(alert => alert.id === alertId), 1);
+
+                   this.setState({ alerts: alerts, open: alerts.length > 0 });
+                 })
+                 .catch(error => { Alert.error(error.response.data.message); });
+
+  }
+
+  render() {
+    return (
+      <Modal open={ this.state.open }
+             title={ LocalizationService.formatMessage('alerts.alerts') }
+             modalName="alerts"
+             modalClass="alerts-modal"
+             closeButtonPosition="right"
+             toggleModal={ this.toggleModal } >
+        {
+          this.state.alerts.map(alert => {
+            return (
+              <div key={ alert.id } className="alert-modal-row">
+                <div style={ { backgroundImage: `url(${alert.image})` } } ></div>
+                <div> { alert.message } </div>
+                <div> <img src={ checkIcon } alt="check_icon" onClick={ () => { this.markAlertAsRead(alert.id) } } /></div>
+              </div>
+            )
+          })
+        }
+      </Modal>
+    );
+  }
+}
--- src/components/dashboard/dashboard_controller.js
+++ src/components/dashboard/dashboard_controller.js
@@ -22,7 +22,7 @@ export default class DashboardController extends Component {
     this.refreshData = this.refreshData.bind(this);
   }
 
-  componentWillMount() {
+  componentDidMount() {
     this.refreshData();
   }
 
