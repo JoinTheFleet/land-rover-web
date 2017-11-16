614c9e03387e8ee4048e145199a07a496064e1f8
Michael Dever <michael@showoff.ie>
2017-11-16 18:10:39 +0000

    List your car toggles registration

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -42,7 +42,7 @@ export default class Header extends Component {
         <LocationPeriodFilter {...this.props} hideSearchForm={ hideSearchForm } />
 
         <div className={'pull-right hidden-xs header-right-options' + (this.props.loggedIn ? ' hide' : '') }>
-          <a id="header_list_car_link" className="header-right-option static-link white-text" onClick={ () => { this.props.handleMenuItemSelect('listings'); }}>List your car</a>
+          <a id="header_list_car_link" className="header-right-option static-link white-text" onClick={ () => { this.toggleModal('registration'); }}>List your car</a>
           <a id="header_login_link" className="header-right-option static-link white-text" onClick={ () => { this.toggleModal('login'); }}>Log in</a>
           <a id="header_register_link" className="header-right-option static-link white-text" onClick={ () => { this.toggleModal('registration'); }}>Sign up</a>
         </div>
