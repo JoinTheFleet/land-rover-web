d033c6d570aaf5d0e9aa12464f248b9f4efefe4a
Pedro Miguel Dias de Matos <pedro@showoff.ie>
2017-11-20 16:28:26 +0000

    Disabled Country of Residence field after being saved. Fixed an issue with the country select overlapping the other inputs.

--- src/components/user_management/user_profile_verified_info/user_form.js
+++ src/components/user_management/user_profile_verified_info/user_form.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { injectIntl } from 'react-intl';
+import PropTypes from 'prop-types';
 
 import FormRow from '../../miscellaneous/forms/form_row';
 import FormGroup from '../../miscellaneous/forms/form_group';
@@ -14,7 +15,7 @@ class UserForm extends Component {
     this.state = {
       open: false,
       type: undefined
-    }
+    };
 
     this.genderOptions = [
       { value: 'male', label: this.props.intl.formatMessage({id: 'user_profile_verified_info.gender.male'}) },
@@ -54,20 +55,24 @@ class UserForm extends Component {
     if (!country && this.props.user.address.country) {
       country = this.props.user.address.country.alpha2;
     }
-    
+
     return (
-      <div>
+      <div className="user-form">
         <FormRow type='singleyeardate' id='user-dateofbirth' handleChange={ this.props.handleDateChange } value={ this.props.user.date_of_birth ? moment.unix(this.props.user.date_of_birth) : moment() } placeholder={ this.props.intl.formatMessage({id: 'user_profile_verified_info.date_of_birth'}) } />
         <FormRow type='select' id='user-gender' clearable={ false } handleChange={ this.props.handleGenderChange } value={ this.props.user.gender } options={ this.genderOptions } placeholder={ this.props.intl.formatMessage({id: 'user_profile_verified_info.gender'}) } />
-        <FormRow type='country' id='user-country' clearable={ false } handleChange={ this.props.handleUserCountryChange } value={ this.props.user.country_code || (this.props.user && this.props.user.country ? this.props.user.country.alpha2 : undefined) } placeholder={ this.props.intl.formatMessage({id: 'user_profile_verified_info.country_of_residence' })} />
+        <FormRow type='country' id='user-country' disabled={ this.props.fieldsToDisable.countryOfResidence } clearable={ false } handleChange={ this.props.handleUserCountryChange } value={ this.props.user.country_code || (this.props.user && this.props.user.country ? this.props.user.country.alpha2 : undefined) } placeholder={ this.props.intl.formatMessage({id: 'user_profile_verified_info.country_of_residence' })} />
 
         <FormGroup placeholder={ this.props.intl.formatMessage({id: 'user_profile_verified_info.address'}) }>
-          <FormField id='user-address-line1' handleChange={ this.props.handleAddressLine1Change } type='text' value={ this.props.user.address.line1 } placeholder={ this.props.intl.formatMessage({id: 'user_profile_verified_info.address.address_line_1'})}/>
-          <FormField id='user-address-line2' handleChange={ this.props.handleAddressLine2Change } type='text' value={ this.props.user.address.line2 } placeholder={ this.props.intl.formatMessage({id: 'user_profile_verified_info.address.address_line_2'})}/>
-          <FormField id='user-address-city' handleChange={ this.props.handleCityChange } type='text' value={ this.props.user.address.city } placeholder={ this.props.intl.formatMessage({id: 'user_profile_verified_info.address.state'})}/>
-          <FormField id='user-address-state' handleChange={ this.props.handleStateChange } type='text' value={ this.props.user.address.state } placeholder={ this.props.intl.formatMessage({id: 'user_profile_verified_info.address.county'})}/>
-          <FormField id='user-address-postcode' handleChange={ this.props.handlePostCodeChange } type='text' value={ this.props.user.address.postal_code } placeholder={ this.props.intl.formatMessage({id: 'user_profile_verified_info.address.post_code'})}/>
-          <FormField id='user-address-country' handleChange={ this.props.handleCountryChange } type='country' value={ country } clearable={ false } placeholder={ this.props.intl.formatMessage({id: 'user_profile_verified_info.address.country'})}/>
+          <div className="col-xs-12 no-side-padding">
+            <FormField id='user-address-line1' handleChange={ this.props.handleAddressLine1Change } type='text' value={ this.props.user.address.line1 } placeholder={ this.props.intl.formatMessage({id: 'user_profile_verified_info.address.address_line_1'})}/>
+            <FormField id='user-address-line2' handleChange={ this.props.handleAddressLine2Change } type='text' value={ this.props.user.address.line2 } placeholder={ this.props.intl.formatMessage({id: 'user_profile_verified_info.address.address_line_2'})}/>
+            <FormField id='user-address-city' handleChange={ this.props.handleCityChange } type='text' value={ this.props.user.address.city } placeholder={ this.props.intl.formatMessage({id: 'user_profile_verified_info.address.state'})}/>
+            <FormField id='user-address-state' handleChange={ this.props.handleStateChange } type='text' value={ this.props.user.address.state } placeholder={ this.props.intl.formatMessage({id: 'user_profile_verified_info.address.county'})}/>
+            <FormField id='user-address-postcode' handleChange={ this.props.handlePostCodeChange } type='text' value={ this.props.user.address.postal_code } placeholder={ this.props.intl.formatMessage({id: 'user_profile_verified_info.address.post_code'})}/>
+          </div>
+          <div className="col-xs-12 no-side-padding">
+            <FormField id='user-address-country' handleChange={ this.props.handleCountryChange } type='country' value={ country } clearable={ false } placeholder={ this.props.intl.formatMessage({id: 'user_profile_verified_info.address.country'})}/>
+          </div>
         </FormGroup>
 
         <FormRow type='text' id='user-email' handleChange={ this.props.handleEmailChange } value={ this.props.user.email } placeholder={ this.props.intl.formatMessage({id: 'user_profile_verified_info.email'}) } />
@@ -81,3 +86,7 @@ class UserForm extends Component {
 }
 
 export default injectIntl(UserForm)
+
+UserForm.propTypes = {
+  fieldsToDisable: PropTypes.object
+}
