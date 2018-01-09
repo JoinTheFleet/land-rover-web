b0233c5b52106acbf9331e4e3a5ae3fc7145e7c7
Michael Dever <michael@showoff.ie>
2018-01-09 13:01:38 +0000

    Only select images for user profile image

--- src/components/user_management/user_profile/user_image.js
+++ src/components/user_management/user_profile/user_image.js
@@ -12,7 +12,7 @@ export default class UserImage extends Component {
           <div className='col-xs-12 form-body'>
             { this.props.imageURL ? <img className='col-xs-12 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4 panel-image' alt='' src={ this.props.imageURL}/> : '' }
 
-            <input id='imageInput' type='file' onChange={ this.props.handleImageFile } style={{ display: 'none'}} ref={(ref) => this.imageInput = ref} />
+            <input id='imageInput' type='file' accept="image/*" onChange={ this.props.handleImageFile } style={{ display: 'none'}} ref={(ref) => this.imageInput = ref} />
 
             <button className='btn btn-primary text-center col-xs-12' onClick={ (event) => this.imageInput.click() }>
               <FormattedMessage id="user_profile.upload_profile_pic" />
