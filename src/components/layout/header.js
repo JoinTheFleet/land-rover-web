10c7d7cac6041be9fd65b37e0f8cf9f5072adad9
Michael Dever <michael@showoff.ie>
2018-01-09 16:03:00 +0000

    Show list your car button on mobile

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -66,10 +66,10 @@ export default class Header extends Component {
 
         { mobileSearchIcon }
 
-        <div className={'pull-right hidden-xs header-right-options' + (this.props.loggedIn ? ' hide' : '') }>
+        <div className={'pull-right header-right-options' + (this.props.loggedIn ? ' hide' : '') }>
           <a id="header_list_car_link" className="header-right-option static-link white-text" onClick={ () => { this.toggleModal('registration'); }}> { LocalizationService.formatMessage('header.list_your_car') } </a>
-          <a id="header_login_link" className="header-right-option static-link white-text" onClick={ () => { this.toggleModal('login'); }}> { LocalizationService.formatMessage('header.log_in') } </a>
-          <a id="header_register_link" className="header-right-option static-link white-text" onClick={ () => { this.toggleModal('registration'); }}> { LocalizationService.formatMessage('header.sign_up') } </a>
+          <a id="header_login_link" className="hidden-xs header-right-option static-link white-text" onClick={ () => { this.toggleModal('login'); }}> { LocalizationService.formatMessage('header.log_in') } </a>
+          <a id="header_register_link" className="hidden-xs header-right-option static-link white-text" onClick={ () => { this.toggleModal('registration'); }}> { LocalizationService.formatMessage('header.sign_up') } </a>
         </div>
 
         <HeaderMenu loggedIn={ this.props.loggedIn }
