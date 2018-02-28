eb8c5b326abced155295ea67f6cc733f17113a7a
Michael Dever <michael@showoff.ie>
2018-02-28 16:02:53 +0000

    Tidy modal close button

--- src/components/users/user_verification_modal.js
+++ src/components/users/user_verification_modal.js
@@ -12,17 +12,24 @@ export default class UserVerificationModal extends Component {
 
     this.state = {
       currentStepNumber: 1,
-      verificationSteps: []
+      verificationSteps: [],
+      componentUpdated: undefined
     }
     
     this.showRenterVerifications = this.showRenterVerifications.bind(this);
     this.buildRenterVerificationSteps = this.buildRenterVerificationSteps.bind(this);
     this.buildOwnerVerificationSteps = this.buildOwnerVerificationSteps.bind(this);
+    this.setVerificationComponent = this.setVerificationComponent.bind(this);
     this.nextStep = this.nextStep.bind(this);
   }
 
   showRenterVerifications() {
     return this.props.scope === 'renter';
+  }
+
+  setVerificationComponent(verificationComponent) {
+    this.verificationComponent = verificationComponent;
+    this.setState({ verificationComponentUpdated: Date.now() });
   }
 
   componentWillMount() {
@@ -70,13 +77,14 @@ export default class UserVerificationModal extends Component {
   render() {
     if (this.state.currentStepNumber <= this.state.verificationSteps.length) {
       let CurrentVerificationStep = this.state.verificationSteps[this.state.currentStepNumber - 1];
-      let currentVerificationStep = <CurrentVerificationStep ref='currentVerificationStep' />;
-      let disabledNext = !this.refs.currentVerificationStep || !this.refs.currentVerificationStep.verified();
+      let currentVerificationStep = <CurrentVerificationStep ref={ this.setVerificationComponent } />;
+      let disabledNext = !this.verificationComponent || !this.verificationComponent.verified();
       let stepWidth = 100.0 / ((this.state.verificationSteps.length - 1) || 1);
       let ulClass = this.state.verificationSteps.length <= 1 ? 'single' : '';
+      let modalTitle = !this.verificationComponent ? '' : this.verificationComponent.title();
 
       return (
-        <Modal {...this.props} modalClass='user-verification' hideCloseButton>
+        <Modal {...this.props} modalClass='user-verification' title={ modalTitle }>
           <div className='row'>
             <div className='col-xs-12 verification'>
               { currentVerificationStep }
@@ -113,7 +121,7 @@ export default class UserVerificationModal extends Component {
                   }
                 </ul>
               </div>
-              <button type='button' className='col-xs-2 btn button text-center' onClick={ this.nextStep } disabled={ false } >
+              <button type='button' className='col-xs-2 btn button text-center' onClick={ this.nextStep } disabled={ disabledNext } >
                 Next
               </button>
             </div>
