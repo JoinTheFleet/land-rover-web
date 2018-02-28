443e7c4f7fb160169564469516e9f2ed18dd9bb9
Michael Dever <michael@showoff.ie>
2018-02-28 14:47:23 +0000

    Remove logs

--- src/components/users/verification_steps/profile_information_verification.js
+++ src/components/users/verification_steps/profile_information_verification.js
@@ -3,12 +3,10 @@ import React, { Component } from 'react';
 
 export default class ProfileInformationVerification extends Component {
   verified() {
-    console.log('called')
     return false;
   }
 
   render() {
-    console.log('rendering')
     return (<div>ProfileInformationVerification</div>);
   }
 }
--- src/components/users/verification_steps/verified_information_verification.js
+++ src/components/users/verification_steps/verified_information_verification.js
@@ -3,12 +3,10 @@ import React, { Component } from 'react';
 
 export default class VerifiedInformationVerification extends Component {
   verified() {
-    console.log('called')
     return false;
   }
 
   render() {
-    console.log('rendering')
     return (<div>VerifiedInformationVerification</div>);
   }
 }
