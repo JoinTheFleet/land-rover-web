3d0e0ad7c0cc1908cdb9950557d6c66c38eafbda
Michael Dever <michael@showoff.ie>
2018-05-31 15:54:24 +0100

    Display link to booking from conversation on web

--- src/components/miscellaneous/forms/form_panel.js
+++ src/components/miscellaneous/forms/form_panel.js
@@ -3,12 +3,23 @@ import PropTypes from 'prop-types';
 
 export default class FormPanel extends Component {
   render() {
+    let button = '';
+
+    if (this.props.button) {
+      button = (
+        <div>
+          { this.props.button }
+        </div>
+      )
+    }
+
     return (
       <div className={ `${this.props.className} col-xs-12 no-side-padding` }>
         <div className='col-xs-12 no-side-padding'>
           <div className="panel-form row">
             <div className='col-xs-12 form-header'>
               { this.props.title }
+              { button }
             </div>
             <div className='col-xs-12 form-body'>
               { this.props.children }
